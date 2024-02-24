f1bcdf5867c1580a536903893d95c4d208cff0cd
genesis <3207595064@qq.com>
2024-02-24 20:56:25 +0800

    lazy lab(copyin copyout)

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,26 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if ((r_scause() == 13) || (r_scause() == 15)) {
+    // 返回出错的虚拟地址
+    uint64 va = r_stval();
+    // 不能添加printf, 否则答案不正确
+    // va地址>=p->sz, 或<stack
+    if ((va >= p->sz) || (va < p->trapframe->sp)) {
+      p->killed = 1;
+    } else {
+      uint64 ka = (uint64) kalloc();
+      if (ka == 0) {
+        p->killed = 1;
+      } else {
+        memset((void*)ka, 0, PGSIZE);
+        va = PGROUNDDOWN(va);
+        if(mappages(p->pagetable, va, PGSIZE, ka, PTE_W|PTE_R|PTE_U) != 0) {
+          kfree((void*)ka);
+          p->killed = 1;
+        }
+      }
+    }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
--- kernel/vm.c
+++ kernel/vm.c
@@ -5,6 +5,8 @@
 #include "riscv.h"
 #include "defs.h"
 #include "fs.h"
+#include "spinlock.h"
+#include "proc.h"
 
 /*
  * the kernel's page table.
@@ -105,6 +107,19 @@ walkaddr(pagetable_t pagetable, uint64 va)
     return 0;
   if((*pte & PTE_V) == 0)
     return 0;
+//  if (pte == 0 || (*pte & PTE_V) == 0) {
+//   struct proc *p = myproc();
+//    if (va >= p->sz || va < PGROUNDUP(p->trapframe->sp))
+//      return 0;
+//    uint64 ka = (uint64)kalloc;
+//    if (ka == 0)
+//      return 0;
+//    if (mappages(p->pagetable, PGROUNDDOWN(va), PGSIZE, ka, PTE_W | PTE_X | PTE_U | PTE_R) != 0) {
+//      kfree((void*)ka);
+//      return 0;
+//    }
+//    return ka;
+//  }
   if((*pte & PTE_U) == 0)
     return 0;
   pa = PTE2PA(*pte);
@@ -181,9 +196,11 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
+      continue;
+      //panic("uvmunmap: walk");
     if((*pte & PTE_V) == 0)
-      panic("uvmunmap: not mapped");
+      continue;//just ok
+//      panic("uvmunmap: not mapped");
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
@@ -315,9 +332,11 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
-      panic("uvmcopy: pte should exist");
+      continue;
+      //panic("uvmcopy: pte should exist");
     if((*pte & PTE_V) == 0)
-      panic("uvmcopy: page not present");
+      continue;
+      //panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
@@ -355,12 +374,27 @@ int
 copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
 {
   uint64 n, va0, pa0;
-
+  struct proc *p = myproc();
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
     pa0 = walkaddr(pagetable, va0);
-    if(pa0 == 0)
-      return -1;
+    if (pa0 == 0) {
+      if ((va0 >= p->sz) || (va0 < p->trapframe->sp)) {
+        return -1;
+      } else {
+        pa0 = (uint64)kalloc();
+        if (pa0 == 0) {
+          p->killed = 1;
+        } else {
+          memset((void *)pa0, 0, PGSIZE);
+          va0 = PGROUNDDOWN(va0);
+          if(mappages(p->pagetable, va0, PGSIZE, pa0, PTE_W|PTE_R|PTE_U) != 0) {
+            kfree((void *)pa0);
+            p->killed = 1;
+          }
+        }
+      }
+    }
     n = PGSIZE - (dstva - va0);
     if(n > len)
       n = len;
@@ -380,12 +414,27 @@ int
 copyin(pagetable_t pagetable, char *dst, uint64 srcva, uint64 len)
 {
   uint64 n, va0, pa0;
-
+  struct proc *p = myproc();
   while(len > 0){
     va0 = PGROUNDDOWN(srcva);
     pa0 = walkaddr(pagetable, va0);
-    if(pa0 == 0)
-      return -1;
+    if (pa0 == 0) {
+      if ((va0 >= p->sz) || (va0 < p->trapframe->sp)) {
+        return -1;
+      } else {
+        pa0 = (uint64) kalloc();
+        if (pa0 == 0) {
+          p->killed = 1;
+        } else {
+          memset((void *)pa0, 0, PGSIZE);
+          va0 = PGROUNDDOWN(va0);
+          if(mappages(p->pagetable, va0, PGSIZE, pa0, PTE_W|PTE_R|PTE_U) != 0) {
+            kfree((void *)pa0);
+            p->killed = 1;
+          }
+        }
+      }
+    }
     n = PGSIZE - (srcva - va0);
     if(n > len)
       n = len;
