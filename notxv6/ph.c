ee164a5163c320be7817e3a03bfe44036a37152d
genesis <3207595064@qq.com>
2024-03-11 09:25:47 +0800

    get recover

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -72,11 +72,9 @@ get(int key)
 
 
   struct entry *e = 0;
-  pthread_mutex_lock(&lock[i]); 
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-  pthread_mutex_unlock(&lock[i]);
 
   return e;
 }
